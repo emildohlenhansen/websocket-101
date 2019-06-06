616be4875c659784f0bd34d789a52d2abc5fddb7
Karl Evald Wigestrand <karl.wigestrand@bekk.no>
2019-06-06 18:38:33 +0200

    Game: Optimaliserte antall kall til serveren, la på fargehashing og endret litt diverse logikk

--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -5,20 +5,19 @@ const Game = ({ socket }) => {
   const [xPostion, setXPostion] = useState(0);
   const [yPostion, setyPostion] = useState(0);
   const { id } = socket;
+  const updateRate = 1;
 
   socket.on('positions', positions => {
     //empty canvas
-    console.log(positions);
     const canvas = document.getElementById('canvas');
     const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
     positions.forEach(position => {
-      if (position.id === id) {
-        context.clearRect(0, 0, canvas.width, canvas.height);
+      if (position.id) {
+        context.fillStyle = intToRGB(hashCode(position.id));
+        context.fillRect(position.x, position.y, 10, 5);
       }
-      context.fillStyle = `#${Math.floor(Math.random() * 16777215).toString(
-        16
-      )}`; //random hex
-      context.fillRect(position.x, position.y, 10, 5);
     });
   });
 
@@ -33,10 +32,26 @@ const Game = ({ socket }) => {
     const x = (clientX - rect.left) * scaleX;
     const y = (clientY - rect.top) * scaleY;
 
-    setXPostion(x);
-    setyPostion(y);
+    if(Math.abs(xPostion - x) > updateRate || Math.abs(yPostion - y) > updateRate) {
+      setXPostion(x);
+      setyPostion(y);
+      socket.emit('updatePosition', { id, x, y });
+    }
+  };
 
-    socket.emit('updatePosition', { id, x, y });
+  const hashCode = str => {
+    // java String#hashCode
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return hash;
+  };
+
+  const intToRGB = i => {
+    var c = (i & 0x00ffffff).toString(16).toUpperCase();
+
+    return "#" + "00000".substring(0, 6 - c.length) + c;
   };
 
   return (
